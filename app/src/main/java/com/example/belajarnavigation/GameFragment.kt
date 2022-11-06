5842559dde13c8bf3a877dfaa893016eb33d2370
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-06 21:44:48 +0700

    Preparing navigation resources that contains Nav Graph. Implementing all dependencies needed in build.gradle. Using NavHostFragment to host Fragment. And implementing navigation between fragments.

--- app/src/main/java/com/example/belajarnavigation/GameFragment.kt
+++ app/src/main/java/com/example/belajarnavigation/GameFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_game.*
 
 class GameFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +15,15 @@ class GameFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonBack.setOnClickListener {
+            // Get which action to run
+            val action = GameFragmentDirections.actionMainFragment()
+            // Send action to Navigation.navController
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
--- app/src/main/java/com/example/belajarnavigation/MainFragment.kt
+++ app/src/main/java/com/example/belajarnavigation/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +15,15 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonStart.setOnClickListener {
+            // Get which action to run
+            val action = MainFragmentDirections.actionGameFragment()
+            // Send action to Navigation.navController
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
